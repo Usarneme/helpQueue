fce586d9cf7f0e4bcd859c5b7f0b455e21d0d6e4
Usarneme <jamestcoop@gmail.com>
2021-07-06 14:08:07 -0700

    :tada: refactors new ticket form to use react hooks for controlled inputs
    Co-authored-by: Nick Reeder <nickreeder32@gmail.com>

--- help-queue/src/components/NewTicketForm.js
+++ help-queue/src/components/NewTicketForm.js
@@ -1,19 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 function NewTicketForm(props) {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
   const formSubmissionHandler = event => {
     event.preventDefault()
-    const title = event.target.title.value;
-    const description = event.target.description.value;
+    // const title = event.target.title.value;
+    // const description = event.target.description.value;
     console.log("form submitted", title, description)
     props.onNewTicketCreation(title, description)
+    // event.target.title.value = ""
+    // event.target.description.value = ""
+    setTitle("")
+    setDescription("")
   }
 
   return (
     <form onSubmit={event => formSubmissionHandler(event)}>
-      <input type="text" name="title" placeholder="title" />
-      <input type="text" name="description" placeholder="description" />
+      <input type="text" name="title" placeholder="title" value={title} onChange={e => setTitle(e.target.value)} />
+      <input type="text" name="description" placeholder="description" value={description} onChange={e => setDescription(e.target.value)} />
       <button type="submit">Add new ticket</button>
     </form>
   )
