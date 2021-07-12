da180efc31dbbb75c15790b0fcd5e85ac339413a
Usarneme <jamestcoop@gmail.com>
2021-07-11 20:13:11 -0700

    :recycle: refactors tickets component from class to function as crud is being moved to firebase hooks

--- help-queue/src/components/Tickets.js
+++ help-queue/src/components/Tickets.js
@@ -1,76 +1,25 @@
-import React, { Component } from 'react'
-import { v4 as uuid } from 'uuid'
+import React, { useState } from 'react'
 import { withFirestore } from 'react-redux-firebase'
 
 import Kanban from './Kanban'
 import NewTicketForm from './NewTicketForm'
-import { ADD, UPDATE, DELETE } from './../actions/index'
 
-class Tickets extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      newTicketFormShowing: false
-    }
+function Tickets(props) {
+  const [newTicketFormShowing, showNewTicketForm] = useState(false)
+
+  const handleShowNewTicketFormButtonClick = () => {
+    showNewTicketForm(!newTicketFormShowing)
   }
 
-  // DEPRECATED after introduction of Firebase Hook in NewForm component
-  addNewTicket = (title, description) => {
-    const id = uuid()
-    const { dispatch } = this.props;
-    const action = {
-      type: ADD,
-      id: id,
-      title: title,
-      description: description,
-      status: "Todo"
-    }
-    dispatch(action)
-  }
-
-  // DEPRECATED - replaced with useFirestore hook in ticket component
-  changeTicketStatus = (ticket, newStatus) => {
-    // const { dispatch } = this.props;
-    // const { id, title, description } = ticket;
-    // const action = {
-    //   type: UPDATE,
-    //   id: id,
-    //   title: title,
-    //   description: description,
-    //   status: newStatus
-    // }
-    // dispatch(action)
-  }
-
-  deleteTicket = (id) => {
-    const { dispatch } = this.props;
-    const action = {
-      type: DELETE,
-      id: id
-    }
-    dispatch(action)
-  }
-
-  handleShowNewTicketFormButtonClick = () => {
-    this.setState({ newTicketFormShowing: !this.state.newTicketFormShowing })
-    // this.setState(prevState => ({ newTicketFormShowing: !prevState.newTicketFormShowing }))
-  }
-
-  render() {
-    return (
-      <React.Fragment>
-        { this.state.newTicketFormShowing ? <NewTicketForm onNewTicketCreation={this.addNewTicket} /> : null }
-        <button onClick={() => this.handleShowNewTicketFormButtonClick()} >
-          { this.state.newTicketFormShowing ? "Cancel New Ticket" : "Add New Ticket" }
-        </button>
-        <Kanban
-          changeTicketStatus={this.changeTicketStatus}
-          deleteTicket={this.deleteTicket} />
-      </React.Fragment>
-    )
-  }
+  return (
+    <React.Fragment>
+      { newTicketFormShowing ? <NewTicketForm /> : null }
+      <button onClick={() => handleShowNewTicketFormButtonClick()} >
+        { newTicketFormShowing ? "Cancel New Ticket" : "Add New Ticket" }
+      </button>
+      <Kanban />
+    </React.Fragment>
+  )
 }
 
-Tickets = withFirestore(Tickets)
-
-export default Tickets
+export default withFirestore(Tickets)
